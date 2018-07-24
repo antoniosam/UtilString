b3ffb51def1cbad0f5d2c3950bd0f6c423efbc1e
Marco Samano <antoniost.developer@gmail.com>
2018-07-24 14:10:13 -0500

    Class BuildPassword
    Class BuildPassword

--- src/UniqueId.php
+++ src/UniqueId.php
@@ -7,7 +7,7 @@ class UniqueId
 
     static public function generate($clave = null, $random = null)
     {
-        $r = is_null($random) ? time() : $random;
+        $r = is_null($random) ? microtime(true) : $random;
         if (is_null($clave)) {
             return Base62::codeNumber($r);
         }
